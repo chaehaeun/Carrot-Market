98910501d698aefcad22570043eb86496489d63b
chaehaeun <chae_3210@naver.com>
2023-04-30 21:53:04 +0900

    [add] react-hoot-form 유효성 검사 기능

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { useForm, FieldErrors } from "react-hook-form";
 
 // Less code (c)
 // Better validation
@@ -8,30 +8,50 @@ import { useForm } from "react-hook-form";
 // Dont deal with events (c)
 // Easier Inputs (c)
 
+interface LoginForm {
+  username: string;
+  email: string;
+  password: string;
+}
+
 const Form = () => {
-  const { register } = useForm();
+  const { register, handleSubmit } = useForm<LoginForm>();
   // register : input 을 등록하는 함수
   // watch : input(value)을 감시하는 함수
+  // handleSubmit : submit 이벤트를 처리하는 함수
+  // 첫번째 인자로 form이 유효할 때 실행할 함수를 받음, 두번째 인자로 form이 유효하지 않을 때 실행할 함수를 받음
+  // form이 유효할 때 실행할 함수는 form의 데이터를 인자로 받음
+
+  const onValid = (data: LoginForm) => {
+    console.log("valid");
+  };
+
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
-        {...register("username")}
+        {...register("username", {
+          required: "Username is required!",
+          minLength: {
+            message: "Username should be longer than 5 chars.",
+            value: 5,
+          },
+        })}
         type="text"
         placeholder="User name"
-        required
       />
       <input
-        {...register("email")}
+        {...register("email", { required: "Email is required!" })}
         type="email"
         placeholder="User email"
-        required
       />
       <input
-        {...register("password")}
+        {...register("password", { required: "Password is required!" })}
         type="password"
         placeholder="Password"
-        required
       />
       <button type="submit">계정 만들기</button>
     </form>
